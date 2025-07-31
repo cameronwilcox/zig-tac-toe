422ee55d2411769e29dfafdefbdc025d3a424a4e
Cameron Wilcox <cjwilcox15@gmail.com>
2025-07-30 22:08:39 -0700

    Refactor game struct and add error handling

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,15 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 const std = @import("std");
+const stdin = std.io.getStdIn().reader();
 const print = std.debug.print;
 
 pub fn main() !void {
     print("test", .{});
 
+    _ = try start();
 }
 
 const Player = struct{
-    player_num : i32,
+    player_num : u8,
     turns_taken : i32,
 };
 
@@ -19,28 +17,45 @@ const Game = struct {
     player1 : Player,
     player2 : Player,
     current_turn : bool,
+    board : [9]u8,
+    pub fn init(p1 : Player, p2 : Player) Game
+    {
+        return Game {
+            .player1 = p1,
+            .player2 = p2,
+            .current_turn = true,
+            .board = .{'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',},
+        };
+    }
 };
 
-pub fn get_input() []u8
+pub fn get_input(_ : []u8) !void
 {
+    var buf : [2]u8 = undefined;
+    // handle when the user tries to input something too long
+    _ = try stdin.readUntilDelimiterOrEof(&buf, '\n');
+    print("in get_input: {s}", .{buf});
+}
+
+pub fn check_rows(_ : []u8) bool {
+    return false;
+}
+
+pub fn check_cols(_ : []u8) bool {
+    return false;
+}
+
+pub fn turn(_ : Player, board : []u8) !bool {
+    _ = try get_input(board[0..]);
+    // need to first get the input from a player
+        // assign on the board
+    // then need to check for a win
+    return true;
 
 }
 
-pub fn turn(player : Player, board : []u8) bool
-{
-    print("Player.player_num: {}\n", player.player_num);
-
-}
-
-pub fn start() !void
-{
-    var game = Game{
-        .player1 = .{.player_num = 1, .turns_taken = 0},
-        .player2 = .{.player_num = 2, .turns_taken = 0},
-        .current_turn = true,
-    };
-    var board : []u8 = .{'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',};
-
+pub fn start() !void {
+    var game : Game = .init(.{.player_num = '1', .turns_taken = 0}, .{.player_num = '2', .turns_taken = 0});
     print("Game.player1.player_num: {}\n", .{game.player1.player_num});
 
     var game_over : bool = false;
@@ -48,12 +63,12 @@ pub fn start() !void
     {
         if(game.current_turn)
         {
-            game_over = turn(game.player1, board);
+            game_over = try turn(game.player1, game.board[0..]);
             game.current_turn = false;
         }
         else
         {
-            game_over = turn(game.player2, board);
+            game_over = try turn(game.player2, game.board[0..]);
             game.current_turn = true;
         }
 
